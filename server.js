f062391d7f269e56c3982a6c5822536df2dddb67
timonschramm <timonschramm.prod@gmail.com>
2021-09-06 13:44:08 +0200

    added cors

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 require('./config/db')
 
 var express = require('express')
+var cors = require('cors')
 var app = express()
+
+app.use(cors())
 const port = process.env.PORT || 3000;
 
 var router = express.Router()
+
+
 
 const UserRouter = require('./api/User')
 const EventRouter = require('./api/Veranstaltung')
